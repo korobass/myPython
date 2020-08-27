9adbce00151bacf3ca592e18f3671836fc42f61f
Marek Korpacz <marek.korpacz@ipfdigital.com>
2020-08-27 16:31:44 +0200

    Adding static s3 website support.

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,5 +1,7 @@
+
 import boto3
 import click
+from botocore.exceptions import ClientError
 
 session = boto3.Session(profile_name='ipfdigital-poc')
 s3 = session.resource('s3')
@@ -23,6 +25,51 @@ def list_buckets():
     "List all s3 buckets"
     for bucket in s3.buckets.all():
         print(bucket)
+
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    "Create and configure static website on s3"
+    # initiating variable before try/except
+    s3_bucket = None
+    try:
+        s3_bucket = s3.create_bucket(
+            Bucket=bucket,
+            CreateBucketConfiguration={'LocationConstraint': session.region_name}
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            s3_bucket=s3.Bucket(bucket)
+        else:
+            raise e
+
+    policy = """
+    {
+      "Version":"2012-10-17",
+      "Statement":[
+        {
+          "Sid":"PublicReadGetObject",
+          "Effect":"Allow",
+          "Principal": "*",
+          "Action":["s3:GetObject"],
+          "Resource":["arn:aws:s3:::%s/*"]
+          }
+      ]
+    }
+    """ % s3_bucket.name
+    policy = policy.strip()
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+    ws = s3_bucket.Website()
+    ws.put(WebsiteConfiguration={
+            'ErrorDocument': {
+            'Key': 'index.html'
+            },
+            'IndexDocument': {
+            'Suffix': 'index.html'
+            }
+        })
+    return
 
 if __name__ == '__main__':
     cli()
