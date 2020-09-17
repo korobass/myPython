d8f377bfe1d5bbad4619e2d9d0ee088509ae0419
Marek Korpacz <marek.korpacz@ipfdigital.com>
2020-09-17 12:01:41 +0200

    Pipenv refactoring.

--- 01-webotron/ipythonsession.py
+++ 01-webotron/ipythonsession.py
@@ -1,7 +1,9 @@
 # coding: utf-8
+
 import boto3
+
 session = boto3.Session(profile_name='ipfdigital-poc')
 s3 = session.resource('s3')
 acm = session.client('acm')
-#ec2_client = session.client('ec2')
+# ec2_client = session.client('ec2')
 route53_client = session.client('route53')
