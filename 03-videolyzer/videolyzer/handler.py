3614606425b67295cd77d4e5e7b041344b9ea238
marek.korpacz <marek.korpacz@ipfdigital.com>
2020-10-09 09:50:55 +0000

    Trigger rekognition job in lambda function

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,5 +1,25 @@
+import urllib
+import boto3
+from pathlib import Path
+
+def start_label_detection(bucket, key):
+    """Start rekognition for a given s3 key."""
+    rkg_client = boto3.client('rekognition')
+    response = rkg_client.start_label_detection(
+        Video={
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': key
+            }
+        })
+    print(response)
+    return
 
 def start_processing_video(event, context):
     """Rekognition of mp4 video on upload do s3."""
-    print(event)
-
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = urllib.parse.unquote_plus(
+            record['s3']['object']['key']
+        )
+        start_label_detection(bucket, key)
